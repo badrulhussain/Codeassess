17b71e6fb11dbf6ba30316f115ee8f3960ae1acb
badrulhussain <47890161+badrulhussain@users.noreply.github.com>
2019-05-21 23:53:30 +0100

    Added code

---
+++ Codeassess/Codeassess/IntToASCII.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeassess
+{
+    class IntToASCII
+    {
+        static void Main(string[] args)
+        {
+            string unicode = Console.ReadLine();
+
+            int intUnicode = Convert.ToInt32( unicode);
+
+            var charUnicode = Convert.ToChar(intUnicode);
+            if(char.IsUpper(charUnicode))
+            {
+                Console.WriteLine("upper");
+            }
+            if (char.IsLower(charUnicode))
+            {
+                Console.WriteLine("lower");
+            }
+
+        }
+    }
+}
